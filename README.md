57a6b2613b985bc60bcfc8c6d98d23c0df6bafad
Maho Morichika <nat@magicnat.com>
2018-04-02 19:20:40 -0400

    use min_time.

--- README.md
+++ README.md
@@ -6,16 +6,38 @@ cndns 是一个简单的 ChinaDNS 实现。
 ### 用法
 
 ```
-./cndns <bind_addr> <bind_port> <remote_dns> <max_time>
+usage: ./cndns
+   -s <remote_dns>     remote DNS
+   -m <min_time>       minimum respond time (ms)
+   -M [max_time]       lookup timeout (ms) (default: min_time + 1000)
+   -l [bind_addr]      local address (default: 0.0.0.0)
+   -p [bind_port]      local port (default: 53)
+   -v                  verbose
 ```
 
-`bind_addr` = 监听地址；`bind_port` = 监听端口；`remote_dns` = 远端 DNS；`max_time` = 等待时间。例如：
+`-s remote_dns`: 远端 DNS，即想要使用的 DNS 服务器。
+
+`-m min_time`: 最短响应时间，若有短于此时间的 DNS 应答，会被暂时忽略。若有超过此时间的 DNS 应答，会立即返回结果。
+
+`-M max_time`: 最长等待时间，若超过此时间没有新的 DNS 应答，则返回最后接到的应答（默认：min\_time + 1000）。
+
+`-l bind_addr`: 本地 DNS 监听地址（默认：0.0.0.0）。
+
+`-p bind_port`: 本地 DNS 监听端口（默认：53）。
+
+例如：
 
 ```
-./cndns 0.0.0.0 53 1.0.0.1 50
+./cndns -s1.0.0.1 -m15
 ```
 
-会把 DNS 流量转发至 `1.0.0.1`，监听来自 `1.0.0.1` 的应答，直到超过 50 毫秒没有回应。
+会把 DNS 流量转发至 `1.0.0.1`，监听来自 `1.0.0.1` 的应答，并按照下面的行为处理 DNS 应答：
+
+条件|行为
+---|---
+响应时间 < 15ms|暂时忽略
+15ms < 响应时间 < 1015ms|立刻返回
+15ms ~ 1015ms 区间没有应答|返回最后接到的应答
 
 ### 原理
 
@@ -59,4 +81,4 @@ PING 1.0.0.1 (1.0.0.1) 56(84) bytes of data.
 rtt min/avg/max/mdev = 14.591/16.471/18.766/1.846 ms
 ```
 
-那么取 50ms 就是个合理的值。投毒的假结果往往 10 毫秒内就抵达了客户端，低于到目标 DNS 的 RTT。
+那么，15ms 内的应答都不可能是来自真正的目标 DNS 服务器，使用 `-m15` 参数忽略它们。
--- cndns.c
+++ cndns.c
@@ -1,17 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <string.h>
 #include <poll.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define VERSION "0.2"
+
+void help(char *me) {
+    fprintf(stderr, "cndns %s\n\n", VERSION);
+    fprintf(stderr, "usage: %s\n", me);
+    fprintf(stderr, "   -s <remote_dns>     remote DNS\n");
+    fprintf(stderr, "   -m <min_time>       minimum respond time (ms)\n");
+    fprintf(stderr, "   -M [max_time]       lookup timeout (ms) (default: min_time + 1000)\n");
+    fprintf(stderr, "   -l [bind_addr]      local address (default: 0.0.0.0)\n");
+    fprintf(stderr, "   -p [bind_port]      local port (default: 53)\n");
+    fprintf(stderr, "   -v                  verbose\n\n");
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv) {
-    if (argc < 5) {
-        fprintf(stderr, "Usage: %s <bind_addr> <bind_port> <remote_dns> <max_time>\n", argv[0]);
-        exit(1);
+    char *bindaddr = "0.0.0.0", *remoteaddr;
+    bool remoteaddr_set = false, mintime_set = false, debug = false;
+    in_port_t bindport = 53;
+    time_t min_time, max_time = 0;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "s:m:M:l:p:v")) != -1) {
+        switch (opt) {
+            case 's':
+                remoteaddr_set = true;
+                int len = strlen(optarg);
+                remoteaddr = malloc(len);
+                strcpy(remoteaddr, optarg);
+                break;
+            case 'm':
+                mintime_set = true;
+                min_time = atoi(optarg);
+                break;
+            case 'M':
+                max_time = atoi(optarg);
+                break;
+            case 'l':
+                strcpy(bindaddr, optarg);
+                break;
+            case 'p':
+                bindport = atoi(optarg);
+                break;
+            case 'v':
+                debug = true;
+                break;
+            default:
+                help(argv[0]);
+                break;
+        }
     }
+
+    if (!remoteaddr_set || !mintime_set) help(argv[0]);
 
     int s_bind = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
     int s_recv = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
@@ -19,16 +68,16 @@ int main(int argc, char **argv) {
     struct sockaddr_in sin_local, sin_remote;
 
     sin_local.sin_family = AF_INET;
-    sin_local.sin_addr.s_addr = inet_addr(argv[1]);
-    sin_local.sin_port = htons(atoi(argv[2]));
+    sin_local.sin_addr.s_addr = inet_addr(bindaddr);
+    sin_local.sin_port = htons(bindport);
 
     sin_remote.sin_family = AF_INET;
-    sin_remote.sin_addr.s_addr = inet_addr(argv[3]);
+    sin_remote.sin_addr.s_addr = inet_addr(remoteaddr);
     sin_remote.sin_port = htons(53);
 
     if(bind(s_bind, (struct sockaddr *)&sin_local, sizeof(sin_local)) < 0) {
-        fprintf(stderr, "[ERR] Can't bind %s:%s: %s\n", argv[1], argv[2], strerror(errno));
-        exit(1);
+        fprintf(stderr, "[ERR] Can't bind %s:%hu: %s\n", bindaddr, bindport, strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
     struct pollfd fds[1];
@@ -37,18 +86,32 @@ int main(int argc, char **argv) {
     fds[0].events = POLLIN;
     fds[0].revents = 0;
 
-    int max_time = atoi(argv[4]), n;
+    max_time = (max_time ? max_time : min_time + 1000);
+    int n;
 
+    fprintf(stderr, "[INFO] listening on %s:%hu, forwarding to %s, max_time %ldms, min_time %ldms.\n", bindaddr, bindport, remoteaddr, max_time, min_time);
     socklen_t sz;
     char buf[65535];
+    static struct timeval start, end;
+    time_t r_time = 0;
+    int ret;
+
     while(1) {
         sz = sizeof(sin_local);
         n = recvfrom(s_bind, buf, sizeof(buf), 0, (struct sockaddr *)&sin_local, &sz);
         if (n <= 0) continue;
         sendto(s_recv, buf, n, 0, (struct sockaddr *)&sin_remote, sizeof(sin_remote));
         sz = sizeof(sin_remote);
-        while(poll(fds, 1, max_time))
+        r_time = 0;
+        // no clock_gettime(), since I want it to run on OSX.
+        gettimeofday(&start, NULL);
+        while(r_time < min_time && (ret = poll(fds, 1, max_time))) {
             n = recvfrom(s_recv, buf, sizeof(buf), 0, (struct sockaddr *)&sin_remote, &sz);
+            gettimeofday(&end, NULL);
+            r_time = 1000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000;
+            if (debug) fprintf(stderr, "[DEBUG] got a result in %ldms.\n", r_time);
+        }
+        if (debug) fprintf(stderr, "[DEBUG] final result obtained in %ldms, due to %s.\n", r_time, (ret == 0) ? "timeout" : "in range");
         sendto(s_bind, buf, n, 0, (struct sockaddr *)&sin_local, sizeof(sin_local));
     }
 }
